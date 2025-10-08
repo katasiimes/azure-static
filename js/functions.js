4f80d39ab59c5d83b0bdbcc9cbc7a149eba9b283
katasiimes <156089206+katasiimes@users.noreply.github.com>
2025-10-08 12:37:04 +0300

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function toggleAnswers() {
+    const answers = document.getElementById('answers');
+    if (answers.style.display === 'none') {
+        answers.style.display = 'block';
+    } else {
+        answers.style.display = 'none';
+    }
+}
